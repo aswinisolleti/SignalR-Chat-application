63018cfbebb309488691c2275aff1cda158d97bc
Mr_Green <kamlekar.venkatesh@gmail.com>
2012-12-12 11:56:10 +0530

    Updated with correct steps
    I think now I understand how signalr works

--- file.js
+++ file.js
@@ -1,14 +1,17 @@
 //Added References
 
 /*
- * jquery-1.6.4.min.js
- * jquery.signalR-1.0.0-alpha2.min.js
- */
+* jquery-1.8.3.min.js
+* jquery.signalR-1.0.0-alpha2.min.js
+*/
+
 
 var showChatName = new Boolean();
+var connectionID = 1;
 showChatName = true;
 var chatUsername = window.prompt("Enter Username:", "");
 var sameChatName;
+//var users = new chatR.connectedUsersViewModel();
 
 $(function () {
     chatUsername = chatUsername.toLowerCase().replace(/\b[a-z]/g, function (letter) {
@@ -19,17 +22,26 @@ $(function () {
     $('#chat-title-name').text(chatUsername);
     // Proxy created on the fly          
     var chat = $.connection.chat; //calling the created class "Chat"
-    chat.state.username = chatUsername;
-    $.connection.hub.qs = "name=" + chatUsername;
-    
-    chat.client.joins = function (ConnectionId, name, Date) {
-        // Thinking what function should I write here
+
+    //for removing the username who is disconnected from the server
+    chat.client.leave = function (user, date) {
+        user = "";  //making the string empty so that the disconnected user value will be lost.
     };
-    chat.client.leave = function (user, date) { 
-        // Thinking what function should I write here
-    };
-    
-     
+
+
+    $('#txtmsg').keypress(function (e) {
+        if (e.which == 13) {
+            e.preventDefault();
+
+            var message = $('#txtmsg').val();
+            if (message.replace(/\s/g, "").length !== 0) {
+            //calls the server side method
+                chat.server.send(chatUsername, $('#txtmsg').val(), showChatName);
+            }
+            $('#txtmsg').val('');
+        }
+    });
+
     // Declare a function on the chat hub so the server can invoke it          
     chat.client.addMessage = function (chatUsername, message, showChatName) {
 
@@ -45,27 +57,22 @@ $(function () {
         $("#messages").scrollTop($("#messages")[0].scrollHeight);
     };
 
-    $('#txtmsg').keypress(function (e) {
-        if (e.which == 13) {
-            e.preventDefault();
 
-            var message = $('#txtmsg').val();
-            if (message.replace(/\s/g, "").length !== 0) {
-                chat.server.send(chatUsername, $('#txtmsg').val(), showChatName);
-            }
-            $('#txtmsg').val('');
-        }
+    $.connection.hub.start().done(function () {
+        chat.server.joined(chatUsername);
     });
 
-    // Start the connection
-    $.connection.hub.start()
-    .done(function () {
-        chat.server.getConnectedUsers().done(function (users) {
-            for (var i = 0; i < users.Length; ++i) {
-                $('#showUsernames').append(users + '<br>');
-            }
+    chat.client.joins = function () {
+        $('#showUsernames').val('').done(function () {
+            chat.server.getConnectedUsers().done(function (users) {
+                for (var i = 0; i < users.length; ++i) {
+                    $('#showUsernames').append(users[i].Name + '<br>');
+                }
+            });
         });
-    }).done(function () {
-        chat.server.joined();
-    });    
+    };
 });
+
+
+
+        
